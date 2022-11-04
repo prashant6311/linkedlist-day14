b61419507da49f7c6345bff6e7a4a610ba95a97c
prashant6311 <prashantppatil761@gmail.com>
2022-11-04 15:23:07 +0530

    linkedlist

--- MyLinkedListMain.java
+++ MyLinkedListMain.java
@@ -4,9 +4,9 @@ public class MyLinkedListMain {
 
 	public static void main(String[] args) {
 		MyLinkedList<Integer> listNode = new MyLinkedList<>();
-		MyNode<Integer> node = new MyNode<>(70);
-		MyNode<Integer> node2 = new MyNode<>(56);
-		MyNode<Integer> node3 = new MyNode<>(30);
+		MyNode<Integer> node = new MyNode<>(56);
+		MyNode<Integer> node2 = new MyNode<>(30);
+		MyNode<Integer> node3 = new MyNode<>(70);
 		listNode.add(node);
 		listNode.add(node2);
 		listNode.add(node3);
